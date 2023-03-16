94f2ef2c6f575c2ecb3c507ab286deabe89d1925
Macbee280 <56406332+Macbee280@users.noreply.github.com>
2023-03-16 16:04:26 -0700

    Add files via upload

---
+++ CPSC121_1/assignment1.cpp
@@ -0,0 +1,16 @@
+/*
+File: Assignment1.cpp
+Author: Gabe DiMartino
+Date: 2023-01-23
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+ 
+    cout <<"Hello, World" << endl;
+    return 0;
+    
+}
