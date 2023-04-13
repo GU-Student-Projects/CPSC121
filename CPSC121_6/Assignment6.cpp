d614b8a23cf4cbba8ae734f787d686d779d3c0d5
Gabe DiMartino <gdimartino@gabedimartino.com>
2023-04-13 00:27:46 -0700

    Project 6 Push

---
+++ CPSC121_6/Assignment6.cpp
@@ -0,0 +1,166 @@
+/**
+ * Author:
+ * Date Modified
+ * Description:
+*/
+
+#include<iostream>
+#include<time.h>
+#include<chrono>
+#include<thread>
+#include<string>
+#include<iomanip>
+
+#ifdef _WIN32 //This if statement is used to change the library used depending on OS architecture.
+#include <windows.h>
+#else
+#include <cstdlib>
+#endif
+
+using namespace std;
+
+void clearScreen(){ //function to clear terminal
+#ifdef _WIN32
+    system("cls"); //Run system command CLS or CLear
+#else
+    system("clear");
+#endif
+}
+
+using namespace std;
+
+/**
+ * Function:        printReverse(const int NUMBER);
+ * Parameters:      integer NUMBER is the original number we will print in reverse
+ *                  NUMBER must be a positive integer with 1 or more digits
+ * Return:          none (void)
+ * Description:     Print the digits of NUMBER in reverse order.
+ *                  E.g. if NUMBER is 12345, function should print 54321 
+*/
+void printReverse(const int NUMBER){
+    int forwardNumber = NUMBER;
+    int reverseNumber = 0;
+
+    while(NUMBER != 0){
+        reverseNumber *= 10;
+        reverseNumber += forwardNumber%10;
+        forwardNumber /= 10;
+    }
+    cout << "The reverse of " << forwardNumber << " is " << reverseNumber << "."<<endl;
+    this_thread::sleep_for(chrono::seconds(3)); //sleep before clearing screen
+}
+
+
+/**
+ * Function:        oddEvenZero(const int NUMBER);
+ * Parameters:      integer NUMBER
+ *                  NUMBER passed in must be an integer zero or greater
+ * Return:          none (void)
+ * Description:     Count and print how many even, odd, and zero numbers are in
+ *                  NUMBER. Zero does not count toward the even/odd count.
+ *                  E.g. 
+ *                  if NUMBER is 012345, print:
+ *                      Even numbers:   2
+ *                      Odd numbers:    3
+ *                      Zero numbers:   1
+ * 
+ *                  if NUMBER is 0001111, print:
+ *                      Even numbers:   0
+ *                      Odd numbers:    4
+ *                      Zero numbers:   3
+ * 
+ *                  if NUMBER is 23232222, print:
+ *                      Even numbers:   6
+ *                      Odd numbers:    2
+ *                      Zero numbers:   0
+ * 
+ * 
+*/
+void oddEvenZero(const int NUMBER);
+
+/**
+ * Function:        printE(const int NUMBER);
+ * Parameters:      integer NUMBER is the number of terms to calculate E
+ *                  NUMBER passed in must be 1 or greater
+ * Return:          none (void)
+ * Description:     Print the calculated value of E
+ *                  The value of e is the value (1 + 1/n)n
+ *                  In other words e is equal to 1 + 1/1! + 1/2! + 1/3! + 1/4! + 1/5! + 1/6! + 1/7! + ... (etc)
+ *                  For n == 1 e is 2.0000, for n == 2 e is 2.25, for n == 10 e is 2.59374
+ * 
+ *                  NOTE: You can't use arrays, strings or any methods from any classes.  You have to 
+ *                  brute force the answer
+*/
+void printE(const int NUMBER);
+
+/**
+ * Function:        printMenu()
+ * Parameters:      none
+ * Return:          none (void)
+ * Description:     prints the menu to the terminal
+*/
+void printMenu();
+
+int main(){
+
+    bool runProgram = true;
+    string menuChoice;
+
+    do{
+        printMenu();
+        getline(cin, menuChoice);
+        int choice = stoi(menuChoice);
+
+        switch(choice){
+            case 1:
+            {
+                clearScreen();
+                cout << "What number would you like reversed? ";
+                string input;
+                getline(cin, input);
+                int number = stoi(menuChoice);
+                printReverse(number);
+                clearScreen();
+                break;
+            }
+            case 2:
+            {
+                //you implement
+                break;
+            }
+            case 3:
+            {
+                //you implement
+                break;
+            }
+            case 4:
+            {
+                runProgram = false;
+                break;
+            }
+            default:
+                {
+                    cout << "Please enter a valid menu choice... try again" << endl;
+                    break;
+                }
+        }
+
+
+    }while(runProgram);
+
+    return 0;
+}
+
+void printMenu(){
+    string menuPrompt = "";
+    menuPrompt += "-------------------------------------\n";
+    menuPrompt += "1. Print Reverse\n";
+    menuPrompt += "2. Odd Even Zero\n";
+    menuPrompt += "3. Print e\n";
+    menuPrompt += "4. Quit program\n";
+    menuPrompt += "-------------------------------------\n";
+    menuPrompt += "Enter your choice: ";
+    cout << menuPrompt << endl;
+}
+
+//write your functions below here
